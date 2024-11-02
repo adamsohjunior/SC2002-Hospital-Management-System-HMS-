25c2cb757b80d4d44586ea3107b8e1c033d3db2f
adamsohjunior <167672913+adamsohjunior@users.noreply.github.com>
2024-11-03 00:58:59 +0800

    Delete src/Pharmacist.java

--- src/Pharmacist.java
+++
@@ -1,9 +0,0 @@
-public class Pharmacist extends User {
-    public Pharmacist(String id, String name, int age, String gender) {
-        super(id, name, age, gender);
-    }
-    
-    public void displayMenu() {
-
-    }
-}
