d895b17247e0bbb5b5a9bb78fe9f9c0c1efbeabc
Jason-Hp <j4sonho520@gmail.com>
2024-11-16 00:45:17 +0800

    JavaDoc updates for package-info

--- src/model/package-info.java
+++ src/model/package-info.java
@@ -7,21 +7,21 @@
  * </p>
  * <p>Key classes in this package:
  * <ul>
- * <li>{@link model.Administrator} - </li>
+ * <li>{@link model.Administrator} - Represents an administrator who has control over inventory and staff management. </li>
  * <li>{@link model.Appointment} - Manages the details of an appointment between a doctor and a patient.</li>
- * <li>{@link model.AppointmentOutcomeRecord} - </li>
- * <li>{@link model.Availability} - </li>
- * <li>{@link model.Available} - </li>
+ * <li>{@link model.AppointmentOutcomeRecord} - Contains the details of the outcome of the Appoinment (E.g. Diagnosis, Prescription, etc). </li>
+ * <li>{@link model.Availability} - Represent a Date and Time that is able to be scheduled for an Appointment. </li>
+ * <li>{@link model.Available} - Contains a list of available dates (Availability objects) and methods to manage them </li>
  * <li>{@link model.Doctor} - Represents a doctor with associated personal information, medical records, and appointment schedules.</li>
- * <li>{@link model.DoctorRating} - </li>
- * <li>{@link model.Inventory} - </li>
- * <li>{@link model.MedicalRecord} - </li>
+ * <li>{@link model.DoctorRating} - Contains the rating average for a Doctor. </li>
+ * <li>{@link model.Inventory} - Represents a medicine and its stock. </li>
+ * <li>{@link model.MedicalRecord} - Contains all the medical information of a Patient object. </li>
  * <li>{@link model.Patient} - Represents a patient with their personal information and medical records.</li>
- * <li>{@link model.Pharmacist} - </li>
- * <li>{@link model.Prescription} - </li>
- * <li>{@link model.Rating} - </li>
- * <li>{@link model.UpdateRating} - </li>
- * <li>{@link model.User} - </li>
+ * <li>{@link model.Pharmacist} - Represents a pharmacist with storage of medicines and list of all appoinment outcome records to dispense medication. </li>
+ * <li>{@link model.Prescription} - Represents a prescription to be dispensed. </li>
+ * <li>{@link model.Rating} - Contains rating for a User</li>
+ * <li>{@link model.UpdateRating} - To update a User's rating. </li>
+ * <li>{@link model.User} - An abstract class representing a User of the HMS System </li>
  * 
  * </ul>
  * </p>
