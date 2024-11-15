80dc33cf26864a92d6c90e396856305f5fb4fcd7
Tingfeng03 <songtingfeng03@gmail.com>
2024-11-15 21:05:09 +0800

    added info for each package for java doc

---
+++ src/model/package-info.java
@@ -0,0 +1,33 @@
+/**
+ * Contains the data models and business logic of the application.
+ * <p>
+ * This package includes classes representing entities such as users, 
+ * patients, doctors, appointments, and medical records. It also includes 
+ * logic to manage data persistence and updates across different entities.
+ * </p>
+ * <p>Key classes in this package:
+ * <ul>
+ * <li>{@link model.Administrator} - </li>
+ * <li>{@link model.Appointment} - Manages the details of an appointment between a doctor and a patient.</li>
+ * <li>{@link model.AppointmentOutcomeRecord} - </li>
+ * <li>{@link model.Availability} - </li>
+ * <li>{@link model.Available} - </li>
+ * <li>{@link model.Doctor} - Represents a doctor with associated personal information, medical records, and appointment schedules.</li>
+ * <li>{@link model.DoctorRating} - </li>
+ * <li>{@link model.Inventory} - </li>
+ * <li>{@link model.MedicalRecord} - </li>
+ * <li>{@link model.Patient} - Represents a patient with their personal information and medical records.</li>
+ * <li>{@link model.Pharmacist} - </li>
+ * <li>{@link model.Prescription} - </li>
+ * <li>{@link model.Rating} - </li>
+ * <li>{@link model.UpdateRating} - </li>
+ * <li>{@link model.User} - </li>
+ * 
+ * </ul>
+ * </p>
+ *
+ * @since 1.0
+ * @author Song Tingfeng
+ */
+package model;
+
