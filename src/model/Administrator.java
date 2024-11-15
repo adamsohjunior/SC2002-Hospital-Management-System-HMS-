2918cbf4da4ad8c88cfd0286587ff1a15b33f037
Tingfeng03 <songtingfeng03@gmail.com>
2024-11-15 20:25:12 +0800

    addded coment for javadoc

--- src/model/Administrator.java
+++ src/model/Administrator.java
@@ -1,7 +1,6 @@
 package model;
+
 import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 import controller.InventoryManagement;
 import controller.StaffManagement;
 import model.Appointment;
@@ -10,79 +9,113 @@ import view.DisplayLog;
 import view.InputIntChoice;
 import system.SystemManager;
 
+/**
+ * The Administrator class represents an administrator user who has control over inventory and staff management.
+ * It provides methods to manage staff, view appointments, manage inventory, and approve requests.
+ */
 public class Administrator extends User {
 	private InventoryManagement inventoryManagement;
-    private StaffManagement staffManagement;
+	private StaffManagement staffManagement;
 	private ArrayList<Appointment> allAppointments;
 	private InputIntChoice inputIntChoice = new InputIntChoice(7); // 7 input choices
 	private SystemManager systemManager;
 	private ArrayList<User> staffList;
 
-    public Administrator(String id, String name, int age, String gender, ArrayList<Inventory> allInventoryItems, ArrayList<User> staffList, ArrayList<Appointment> allAppointments, SystemManager systemManager) {
-        super(id, name, age, gender);
+	/**
+	 * Constructs an Administrator object with the given details.
+	 *
+	 * @param id               the unique ID of the administrator
+	 * @param name             the name of the administrator
+	 * @param age              the age of the administrator
+	 * @param gender           the gender of the administrator
+	 * @param allInventoryItems the list of all inventory items
+	 * @param staffList        the list of staff members
+	 * @param allAppointments  the list of all appointments
+	 * @param systemManager    the system manager object to manage the system's operations
+	 */
+	public Administrator(String id, String name, int age, String gender, ArrayList<Inventory> allInventoryItems, ArrayList<User> staffList, ArrayList<Appointment> allAppointments, SystemManager systemManager) {
+		super(id, name, age, gender);
 		this.inventoryManagement = new InventoryManagement(allInventoryItems);
-        this.staffManagement = new StaffManagement(staffList, allInventoryItems, allAppointments, systemManager);
+		this.staffManagement = new StaffManagement(staffList, allInventoryItems, allAppointments, systemManager);
 		this.allAppointments = allAppointments;
 		this.systemManager = systemManager;
 		this.staffList = staffList;
-    }
+	}
 
+	/**
+	 * Sets the staff list for the administrator.
+	 *
+	 * @param staffList the new list of staff members
+	 */
 	public void setStaffList(ArrayList<User> staffList) {
-        this.staffList = staffList;
-    }
+		this.staffList = staffList;
+	}
 
-    // Inventory management methods delegated to InventoryManagement
-    public void manageInventory() {
-        inventoryManagement.manageInventory();
-    }
+	/**
+	 * Manages the inventory by delegating to the InventoryManagement class.
+	 */
+	public void manageInventory() {
+		inventoryManagement.manageInventory();
+	}
 
+	/**
+	 * Approves a request for inventory by delegating to the InventoryManagement class.
+	 */
 	public void approveRequest() {
 		inventoryManagement.approveRequest();
 	}
 
-    // Staff management methods delegated to StaffManagement
-    public void manageStaff() {
-        staffManagement.manageStaff();
-    }
-	
-    public void displayMenu() {
-        int choice;
+	/**
+	 * Manages the staff by delegating to the StaffManagement class.
+	 */
+	public void manageStaff() {
+		staffManagement.manageStaff();
+	}
+
+	/**
+	 * Displays the main menu for the administrator and allows interaction
+	 * for managing staff, appointments, inventory, requests, and more.
+	 */
+	public void displayMenu() {
+		int choice;
 		System.out.println("");
 		DisplayLog.display("WELCOME, " + this.getName() + "!");
-		
-		do{
+
+		do {
 			AdminDisplayMenu.display();
 			choice = inputIntChoice.getIntChoice();
-			
+
 			switch(choice) {
-			case 1:
-				manageStaff();
-				break;
-			case 2:
-				viewAppointment(allAppointments);
-				break;
-			case 3:
-				manageInventory();
-				break;
-			case 4:
-				approveRequest();
-				break;
-			case 5:
-				systemManager.shutdown();
-				break;
-			case 6:
-				showInbox();
-				break;
-			case 7:
-			    System.out.println("Logged out successfully!\n");
-				break;
+				case 1:
+					manageStaff();
+					break;
+				case 2:
+					viewAppointment(allAppointments);
+					break;
+				case 3:
+					manageInventory();
+					break;
+				case 4:
+					approveRequest();
+					break;
+				case 5:
+					systemManager.shutdown();
+					break;
+				case 6:
+					showInbox();
+					break;
+				case 7:
+					System.out.println("Logged out successfully!\n");
+					break;
 			}
-			
 		} while (choice != 5 && choice != 7);
-		
-		// scan.close();
-    }
-	
+	}
+
+	/**
+	 * Displays appointment details for all appointments.
+	 *
+	 * @param allAppointments the list of all appointments
+	 */
 	private void viewAppointment(ArrayList<Appointment> allAppointments) {
 		for(Appointment a : allAppointments) {
 			System.out.println("");
@@ -90,6 +123,4 @@ public class Administrator extends User {
 			System.out.println("");
 		}
 	}
-    
 }
-
